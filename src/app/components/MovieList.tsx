8ebd818a5370929045a83dfff4419139480af357
yunyun <imnp516@gmail.com>
2026-02-03 01:26:14 +0700

    update MovieList

--- src/app/components/MovieList.tsx
+++ src/app/components/MovieList.tsx
@@ -1,12 +1,35 @@
 // READ
 import Movie from "./Movie";
+import { MovieType } from "../../../types/movie";
 
-export default async function getMovies(){
-    const movies = await getMovies();
+// 1. Hàm lấy dữ liệu từ TMDB (Chạy trên Server)
 
+async function getMovies(){
+
+    const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+    // Gọi API lấy danh sách phim phổ biến
+    const res = await fetch(    
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=vi-VN&page=1`,
+    )
+
+    if(!res){
+        throw new Error("Can't take film list")
+    }
+
+    return res.json();
+
+}
+
+
+export default async function MovieList(){
+
+    const data = await getMovies();
+    const movies = data.results;
+    
     return(
-        <div>
-            {movies.map(m => <Movie key={m.id} data={m} />)}
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 p-4">
+            {/* Loop for printing movie */}
+            {movies.map((movie: MovieType) => (<Movie key={movie.id} data={movie}/>))}
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-// ✅ ĐÚNG
 import MovieList from "./components/MovieList";
 
 export default function Home() {
   return (
-    <main>
-      <MovieList />
+    <main className="min-h-screen bg-zinc-950 text-white pb-10">
+      {/* Header đơn giản */}
+      <header className="py-6 px-8 border-b border-zinc-800 flex justify-between items-center sticky top-0 bg-zinc-950/90 z-50 backdrop-blur">
+        <h1 className="text-3xl font-extrabold text-red-600 tracking-tighter">
+          MOVIEMASTER
+        </h1>
+        <div className="text-sm text-gray-400">
+          Dự án Next.js CRUD
+        </div>
+      </header>
+
+      {/* Nội dung chính */}
+      <div className="container mx-auto mt-8 px-4">
+        <h2 className="text-2xl font-bold mb-6 flex items-center gap-2">
+          Phim Đang Thịnh Hành
+        </h2>
+        {/* Gọi component MovieList để hiển thị danh sách phim */}
+        <MovieList />
+      </div>
     </main>
-  )
+  );
 }
